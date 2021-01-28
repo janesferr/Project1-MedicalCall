31c95c14a9b25b2f8643d21ccf3fa378cf30d58f
Christopher Kim <chrisnkim1999@gmail.com>
2021-01-28 17:48:47 -0500

    merged incoming changes

---
+++ api-script.js
@@ -0,0 +1,22 @@
+var queryUrl =
+  "http://www.whateverorigin.org/get?url=http://www.communitybenefitinsight.org/api/get_hospitals.php?state=NC";
+$.ajax({
+  url: queryUrl,
+  method: "GET",
+  crossDomain: true,
+  dataType: "jsonp",
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+  },
+}).then(function (response) {
+  console.log(JSON.parse(response.contents));
+
+  //  <input type="submit" class="button secondary" value="Search"
+  $(".button").on("click", function () {
+    var state = "NY";
+    // <input class="input-group-field" type="search" />
+    // var searchInput = $(".input-group-field").val();
+    // <p class="populate-field">
+    $(".populate-field").text(JSON.parse(response.contents[0]));
+  });
+});
