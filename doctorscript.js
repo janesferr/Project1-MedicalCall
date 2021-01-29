46d0c84f19bb6da48e6ad58fa901b270a254de4f
Michelle Smith <smith.michelle.a@gmail.com>
2021-01-29 11:21:33 -0500

    deleted misc files

--- doctorscript.js
+++ doctorscript.js
@@ -7,35 +7,44 @@
 //   3c. Loops through each hospital in hospitals using a forEach() print the details of each hospital as a card.
 
 function click_hospitalSearch(event) {
-    var state = $('#hospital_search_state').val();
-    console.log('the user wants us to search for', state);
+  var state = $("#hospital_search_state").val();
+  console.log("the user wants us to search for", state);
 
-    // construct query
-    var queryUrl = "http://www.whateverorigin.org/get?url=" 
-        + encodeURIComponent("http://www.communitybenefitinsight.org/api/get_hospitals.php?state=" + state);
-    
-    console.log('(fingers crossed) going to request', queryUrl);
+  // construct query
+  var queryUrl =
+    "//http://www.whateverorigin.org/get?url=" +
+    encodeURIComponent(
+      "http://www.communitybenefitinsight.org/api/get_hospitals.php?state=" +
+        state
+    );
 
-    $.ajax({
-        url: queryUrl,
-        method: "GET",
-        dataType: 'jsonp',
-    }).then(function(response) {
-        var hospitals = JSON.parse(response.contents);
-        console.log('we found', hospitals.length, 'hospital(s) in the great state of', state);
-        $('#discoverer').html(`Discover Our Hospitals in ${state} with total number of hospitals as ${hospitals.length}`);
-        renderHospitalCards(hospitals);
-    });
+  console.log("(fingers crossed) going to request", queryUrl);
 
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+    dataType: "jsonp",
+  }).then(function (response) {
+    var hospitals = JSON.parse(response.contents);
+    console.log(
+      "we found",
+      hospitals.length,
+      "hospital(s) in the great state of",
+      state
+    );
+    $("#discoverer").html(
+      `Discover Our Hospitals in ${state} with total number of hospitals as ${hospitals.length}`
+    );
+    renderHospitalCards(hospitals);
+  });
 }
 
 function renderHospitalCards(hospitals) {
+  $("#hospital_row").empty();
 
-    $('#hospital_row').empty();
-
-    hospitals.forEach(hospital => {        
-        $('#hospital_row').append(
-        `<article class="six columns">
+  hospitals.forEach((hospital) => {
+    $("#hospital_row").append(
+      `<article class="six columns">
                 <div class="panel">
                     <h3>${hospital.name}</h3>
                   <p class="hospital-name"> Hospital Name: ${hospital.name}</p>
@@ -45,33 +54,36 @@ function renderHospitalCards(hospitals) {
 
                   <a href="#" class="button secondary small radius">Learn More &raquo;</a>
                 </div>
-          </article>`);
-        
-    });
+          </article>`
+    );
+  });
 }
 
-$('#hospital_search').on("click", click_hospitalSearch);
+$("#hospital_search").on("click", click_hospitalSearch);
 
 function print_state_hospital_info(state) {
-    var queryUrl = "http://www.whateverorigin.org/get?url=" 
-        + encodeURIComponent("http://www.communitybenefitinsight.org/api/get_hospitals.php?state=" + state);
-    
-    console.log('going to request:', queryUrl);
+  var queryUrl =
+    "http://www.whateverorigin.org/get?url=" +
+    encodeURIComponent(
+      "http://www.communitybenefitinsight.org/api/get_hospitals.php?state=" +
+        state
+    );
 
-    $.ajax({
-        url: queryUrl,
-        method: "GET",
-        dataType: 'jsonp',
-    }).then(function(response) {
-        var hospitals = JSON.parse(response.contents);
-        console.log('we found', hospitals.length, 'in the great state of', state);
+  console.log("going to request:", queryUrl);
 
-        // hospitals.forEach(hospital => {
-        //     console.log('Hospital Name:', hospital.name);
-        //     console.log('Street Address:', hospital.street_address);
-        //     console.log('Bed Count:', hospital.hospital_bed_count);
-        //     console.log('--++--');
-        // });
-    });
+  $.ajax({
+    url: queryUrl,
+    method: "GET",
+    dataType: "jsonp",
+  }).then(function (response) {
+    var hospitals = JSON.parse(response.contents);
+    console.log("we found", hospitals.length, "in the great state of", state);
+
+    // hospitals.forEach(hospital => {
+    //     console.log('Hospital Name:', hospital.name);
+    //     console.log('Street Address:', hospital.street_address);
+    //     console.log('Bed Count:', hospital.hospital_bed_count);
+    //     console.log('--++--');
+    // });
+  });
 }
-  
